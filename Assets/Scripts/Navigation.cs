8903e66aab616e126a1ce7653ab3184e488d80b9
Nick Filatov <nick.filatov@cloudcastlegroup.com>
2015-12-13 20:49:49 +0400

    menu scene

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour {
+
+	public Button quit;
+	public Button play;
+
+	void Start () {
+		play = play.GetComponent<Button>();
+		quit = quit.GetComponent<Button>();
+	}
+
+	public void QuitPress () {
+		Application.Quit();
+	}
+
+	public void PlayPress() {
+		Application.LoadLevel(1);
+	}
+
+
+
+}
