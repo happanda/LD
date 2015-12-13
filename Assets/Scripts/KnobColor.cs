0f884e975e989702688527b3d1cc3ee814ca7e65
Pavel Moskalevich <happy.panda.code@gmail.com>
2015-12-13 16:21:05 +0300

    changed prefabs with knobs

---
+++ Assets/Scripts/KnobColor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnobColor : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+
+    void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    public Color color
+    {
+        get { return renderer.color; }
+        set { renderer.color = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
